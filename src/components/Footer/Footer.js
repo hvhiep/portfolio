4a01c7b496d121bbe21164f1ffa787da30d624af
Hoang Van Hiep <hvhiep14.8@gmail.com>
2022-07-27 22:35:22 +0700

    navbar navigation

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import styles from './Footer.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faYoutube, faGithub } from '@fortawesome/free-brands-svg-icons';
 function Footer() {
+    const bannerBtns = [
+        {
+            icon: faLinkedin,
+            link: 'https://github.com/hvhiep',
+        },
+        {
+            icon: faYoutube,
+            link: 'https://github.com/hvhiep',
+        },
+        {
+            icon: faGithub,
+            link: 'https://github.com/hvhiep',
+        },
+    ];
     return (
         <footer className={styles.wrapper}>
             <div className={styles.inner}>
-                <span>footer</span>
+                <div className={styles.title}>Hoang Van Hiep</div>
+                <div className={styles.subtitle}>Frontend Developer</div>
+                <div className={styles.bannerBtnWrapper}>
+                    {bannerBtns.map((item, index) => (
+                        <a key={index} className={styles.bannerBtn} href={item.link} target="_blank" rel="noreferrer">
+                            <FontAwesomeIcon icon={item.icon} color="white" size="1x" />
+                        </a>
+                    ))}
+                </div>
+                <div className={styles.copyright}>Copyright &copy; Hoang Van Hiep</div>
             </div>
         </footer>
     );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import clsx from 'clsx';
 import styles from './Header.module.scss';
-function Header() {
+function Header({ headerColorActive }) {
+
+    const handleNavClick = (event) => {
+        event.preventDefault();
+        const elementName = event.target.innerText.toLowerCase();
+        const destinationElement = document.getElementById(elementName);
+        const headerElement = document.getElementById('header');
+        window.scrollTo(0, destinationElement.offsetTop - headerElement.offsetHeight);
+    }
+
     return (
-        <header className={styles.wrapper}>
+        <header id='header' className={clsx(styles.wrapper, headerColorActive && styles.active)}>
             <div className={styles.inner}>
                 <h1>
-                    <a className={styles.logo} href='#home'>My Portfolio</a>
+                    <a className={styles.logo} href='#home' onClick={handleNavClick}>My Portfolio</a>
                 </h1>
                 <nav className={styles.nav}>
-                    <a className={styles.nav__link} href="#home">Home</a>
-                    <a className={styles.nav__link} href="#education">Education</a>
-                    <a className={styles.nav__link} href="#skills">Skills</a>
-                    <a className={styles.nav__link} href="#projects">Projects</a>
-                    <a className={styles.nav__link} href="#contactme">ContactMe</a>
+                    <a className={styles.nav__link} href="#home" onClick={handleNavClick}>Home</a>
+                    <a className={styles.nav__link} href="#education" onClick={handleNavClick}>Education</a>
+                    <a className={styles.nav__link} href="#skills" onClick={handleNavClick}>Skills</a>
+                    <a className={styles.nav__link} href="#projects" onClick={handleNavClick}>Projects</a>
+                    <a className={styles.nav__link} href="#contactme" onClick={handleNavClick}>ContactMe</a>
                 </nav>
             </div>
         </header>
