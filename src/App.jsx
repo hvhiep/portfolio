42d0f8e14c328ad62242fcf387595dbda6f5caf3
hvhiep14.8 <hvhiep14.8@gmail.com>
2024-05-19 22:28:03 +0700

    fix: test rebase

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className='app'>
-        <Routes>
-          <Route path='/' element={<Home />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+	console.log('--Hello App - test rebase PR');
+	return (
+		<BrowserRouter>
+			<div className='app'>
+				<Routes>
+					<Route
+						path='/'
+						element={<Home />}
+					/>
+				</Routes>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
